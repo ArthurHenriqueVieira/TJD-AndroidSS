85e86f05ca83d736c3452d8f33dd7c80566ebcff
Tutz <ttutty.oliveira@gmail.com>
2013-10-15 12:16:42 -0300

    Adicionando o Botão "sair".

--- src/com/example/tyriangame/MainMenu.java
+++ src/com/example/tyriangame/MainMenu.java
@@ -1,15 +1,33 @@
 package com.example.tyriangame;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainMenu extends Activity {
+	Button btn_sair;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		btn_sair = (Button)findViewById(R.id.btn_sair);
+		
+		//Botão que fecha o jogo
+		if(btn_sair != null) {
+			
+			btn_sair.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					finish();
+					System.exit(0);
+				}
+			});
+		}
 	}
 
 	@Override
