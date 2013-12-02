a110f1d21f883078308ac04c7061dbb3d9afc51f
Tutz <ttutty.oliveira@gmail.com>
2013-12-02 00:34:47 -0200

    Colocando a opção de mudar o volume do jogo

--- gen/com/example/tyriangame/R.java
+++ gen/com/example/tyriangame/R.java
@@ -32,13 +32,18 @@ public final class R {
         public static final int tiro=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int btn_ajuda=0x7f090003;
-        public static final int btn_configuracoes=0x7f090004;
-        public static final int btn_novoJogo=0x7f090001;
-        public static final int btn_ranking=0x7f090002;
-        public static final int btn_sair=0x7f090005;
+        public static final int action_settings=0x7f09000b;
+        public static final int btn_ajuda=0x7f090008;
+        public static final int btn_configuracoes=0x7f090009;
+        public static final int btn_novoJogo=0x7f090006;
+        public static final int btn_ranking=0x7f090007;
+        public static final int btn_sair=0x7f09000a;
         public static final int gameover=0x7f090000;
+        public static final int seek_BarAcele=0x7f090003;
+        public static final int seek_BarSom=0x7f090001;
+        public static final int textView1=0x7f090005;
+        public static final int textView2=0x7f090004;
+        public static final int textView3=0x7f090002;
     }
     public static final class layout {
         public static final int activity_ajuda=0x7f030000;
--- src/com/example/tyriangame/Configuracao.java
+++ src/com/example/tyriangame/Configuracao.java
@@ -1,11 +1,51 @@
 package com.example.tyriangame;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class Configuracao extends Activity {
+	//cria os seeks bar
+	SeekBar seekBarSom;
+	SeekBar seekBarAcele;
+	
+	private AudioManager audioManager;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_configuracoes);
+		
+		seekBarAcele = (SeekBar) findViewById(R.id.seek_BarAcele);
+		seekBarSom = (SeekBar) findViewById(R.id.seek_BarSom);
+		
+		audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		
+		seekBarSom.setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+		seekBarSom.setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_MUSIC));
+		
+		seekBarSom.setOnSeekBarChangeListener(new OnSeekBarChangeListener() 
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) 
+            {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) 
+            {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) 
+            {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                        progress, 0);
+            }
+        });
 	}
 }
